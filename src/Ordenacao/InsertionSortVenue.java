37d59b27623174198d76d6c09b3b258025709121
Deyvid Jerônimo <95938301+dvddoge@users.noreply.github.com>
2023-10-04 21:43:59 +0000

    atualizando insertionSortVenue

--- src/Ordenacao/InsertionSortVenue.java
+++ src/Ordenacao/InsertionSortVenue.java
@@ -1,93 +1,151 @@
-package Ordenacao;
+package Ordenacao.InsertionSort;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class InsertionSortVenue {
-    public static void main(String[] args) {
-        String inputFile = "src/TransformacaoResultados/matches_T2.csv";
-        
-        String path = "src/OrdenacaoResultados/InsertionSort/";
-        
-        String outputMedio = path + "matches_t2_venues_insertionSort_medioCaso.csv";
-        String outputMelhor = path + "matches_t2_venues_insertionSort_melhorCaso.csv";
-        String outputPior = path + "matches_t2_venues_insertionSort_piorCaso.csv";
-        
-        ordenar(inputFile, outputMedio); // Para caso médio
-        
-        // Para melhor caso: suponha que o array já está ordenado, então não faça nada.
-        ordenar(inputFile, outputMelhor); 
-        
-        // Para pior caso: inverta o array depois de ordenado no caso médio e reordene.
-        ordenar(outputMedio, outputPior, true); 
-    }
-    
-    public static void ordenar(String inputFile, String outputFile) {
-        ordenar(inputFile, outputFile, false);
-    }
-    
-    public static void ordenar(String inputFile, String outputFile, boolean inverter) {
-        try (BufferedReader br = new BufferedReader(new FileReader(inputFile));
-            FileWriter writer = new FileWriter(outputFile)) {
 
+    private String inputFile;
+    private String path = "src/OrdenacaoResultados/InsertionSort/";
+    private String outputMedio = path + "matches_t2_venues_insertionSort_medioCaso.csv";
+    private int venueIndex = 7;
+
+    public InsertionSortVenue(String inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    public void ordenar() {
+        ordenarMelhorCaso();
+        ordenarMedioCaso();
+        ordenarPiorCaso();
+    }
+
+    private void ordenarMedioCaso() {
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile));
+             FileWriter writer = new FileWriter(outputMedio)) {
+
+            // Escrever cabeçalho
             String line;
             if ((line = br.readLine()) != null) {
-                writer.write(line + "\n"); // Escreve o cabeçalho no arquivo de saída
+                writer.write(line + "\n");
             }
 
-            int venueIndex = 7; // 'venue' é a 8ª coluna, então o índice é 7
             int rowCount = 0;
-            
-            // Para calcular o número de linhas no arquivo
+
+            // Calcular o número de linhas no arquivo
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
             while (reader.readLine() != null) rowCount++;
             reader.close();
 
-            // Ajustar o tamanho do array baseado no número de linhas
-            String[][] data = new String[rowCount][14]; 
+            String[][] data = new String[rowCount][14];
 
-            rowCount = 0; // Resetar rowCount para reutilizar na leitura das linhas
-            // Ler as linhas restantes e armazenar em um array
+            rowCount = 0;
             while ((line = br.readLine()) != null) {
-                List<String> values = new ArrayList<>();
+                data[rowCount++] = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+            }
+
+            // Embaralhar metade dos dados
+            Random rand = new Random();
+            for (int i = 0; i < rowCount / 2; i++) {
+                int j = rand.nextInt(rowCount / 2);
+                String[] temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+
+            ordenacao(data, venueIndex, rowCount);
+
+            // Escrever no arquivo
+            for (int i = 0; i < rowCount; i++) {
+                writer.write(String.join(",", data[i]) + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void ordenarMelhorCaso() {
+        String outputFile = "src/OrdenacaoResultados/InsertionSort/matches_t2_venues_insertionSort_melhorCaso.csv";
+        ordenacao(inputFile, outputFile, false, true);
+    }
+
+    private void ordenarPiorCaso() {
+        String outputFile = "src/OrdenacaoResultados/InsertionSort/matches_t2_venues_insertionSort_piorCaso.csv";
+        ordenacao(inputFile, outputFile, true, false);
+    }
+
+    private void ordenacao(String[][] data, int venueIndex, int rowCount) {
+        for (int i = 1; i < rowCount; i++) {
+            String[] key = data[i];
+            int j = i - 1;
+    
+            while (j >= 0 && (data[j][venueIndex].isEmpty() || data[j][venueIndex].compareTo(key[venueIndex]) > 0)) {
+                data[j + 1] = data[j];
+                j = j - 1;
+            }
+            data[j + 1] = key;
+        }
+    }    
+
+    private void ordenacao(String inputFile, String outputFile, boolean inverter, boolean melhorCaso) {
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile));
+             FileWriter writer = new FileWriter(outputFile)) {
+
+            String line;
+            if ((line = br.readLine()) != null) {
+                writer.write(line + "\n");
+            }
+
+            int venueIndex = 7;
+            int rowCount = 0;
+
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            while (reader.readLine() != null) rowCount++;
+            reader.close();
+
+            String[][] data = new String[rowCount][14];
+
+            rowCount = 0;
+            while ((line = br.readLine()) != null) {
+                String[] values = new String[14];
                 StringBuilder sb = new StringBuilder();
+                int valueCount = 0;
                 boolean insideQuotes = false;
                 for (char c : line.toCharArray()) {
                     if (c == '"') {
                         insideQuotes = !insideQuotes;
                     } else if (c == ',' && !insideQuotes) {
-                        values.add(sb.toString());
+                        values[valueCount++] = sb.toString();
                         sb.setLength(0);
                     } else {
                         sb.append(c);
                     }
                 }
-                values.add(sb.toString());
-                data[rowCount++] = values.toArray(new String[0]);
+                values[valueCount] = sb.toString();
+                data[rowCount++] = values;
             }
-    
-            // Obter o tempo inicial
-            long startTime = System.currentTimeMillis();
 
-            // Implementação do Insertion Sort
             for (int i = 1; i < rowCount; i++) {
                 String[] key = data[i];
                 int j = i - 1;
-
-                // Tratar corretamente campos 'venue' vazios
-                while (j >= 0 && 
-                    (data[j][venueIndex].isEmpty() || data[j][venueIndex].compareTo(key[venueIndex]) > 0)) {
-                    data[j + 1] = data[j];
-                    j = j - 1;
+        
+                if (melhorCaso) {
+                    while (j >= 0 && data[j][venueIndex].compareTo(key[venueIndex]) > 0) {
+                        data[j + 1] = data[j];
+                        j = j - 1;
+                    }
+                } else {
+                    while (j >= 0 && (data[j][venueIndex].isEmpty() || data[j][venueIndex].compareTo(key[venueIndex]) > 0)) {
+                        data[j + 1] = data[j];
+                        j = j - 1;
+                    }
                 }
                 data[j + 1] = key;
             }
 
-            // Se pior caso, inverter o array
             if (inverter) {
                 String[][] dataInvertida = new String[rowCount][14];
                 for (int i = 0; i < rowCount; i++) {
@@ -96,14 +154,6 @@ public class InsertionSortVenue {
                 data = dataInvertida;
             }
 
-            // Obter o tempo final
-            long endTime = System.currentTimeMillis();
-
-            // Calcular e imprimir a diferença
-            long elapsedTime = endTime - startTime;
-            System.out.println("Tempo de execução para " + outputFile + ": " + elapsedTime + " ms");
-
-            // Escrever as linhas ordenadas no arquivo de saída
             for (int i = 0; i < rowCount; i++) {
                 StringBuilder sb = new StringBuilder();
                 for (int j = 0; j < data[i].length; j++) {
@@ -119,8 +169,9 @@ public class InsertionSortVenue {
                 }
                 writer.write(sb.toString() + "\n");
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }    
+    }
 }
